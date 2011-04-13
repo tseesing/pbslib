97ba56af9d66b9c4aa47cb13b711e8ec87606156
ylmfos_user <ylmfos_user@115.com>
2011-04-13 14:08:53 +0800

    updates

---
+++ sys-apps/linux-kernel/files/aufs2.1-38/fs/aufs/.vfsub.o.d
@@ -0,0 +1,247 @@
+vfsub.o: /root/aufs2-standalone.git/fs/aufs/vfsub.c \
+ include/generated/autoconf.h include/linux/file.h \
+ include/linux/compiler.h include/linux/compiler-gcc.h \
+ include/linux/compiler-gcc4.h include/linux/types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/types.h \
+ include/asm-generic/types.h include/asm-generic/int-ll64.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h include/linux/posix_types.h \
+ include/linux/stddef.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/posix_types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/posix_types_32.h \
+ include/linux/ima.h include/linux/fs.h include/linux/limits.h \
+ include/linux/ioctl.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/ioctl.h \
+ include/asm-generic/ioctl.h include/linux/blk_types.h \
+ include/linux/linkage.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/linkage.h \
+ include/linux/stringify.h include/linux/wait.h include/linux/list.h \
+ include/linux/poison.h include/linux/prefetch.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/processor.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/processor-flags.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/vm86.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/ptrace.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/ptrace-abi.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/segment.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/page_types.h \
+ include/linux/const.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/page_32_types.h \
+ include/linux/init.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/math_emu.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/sigcontext.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/current.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/percpu.h \
+ include/linux/kernel.h \
+ /usr/lib/gcc/i686-pc-linux-gnu/4.4.3/include/stdarg.h \
+ include/linux/bitops.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/bitops.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/alternative.h \
+ include/linux/jump_label.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/asm.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/cpufeature.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/required-features.h \
+ include/asm-generic/bitops/find.h include/asm-generic/bitops/sched.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/arch_hweight.h \
+ include/asm-generic/bitops/const_hweight.h \
+ include/asm-generic/bitops/fls64.h \
+ include/asm-generic/bitops/ext2-non-atomic.h \
+ include/asm-generic/bitops/le.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/byteorder.h \
+ include/linux/byteorder/little_endian.h include/linux/swab.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/swab.h \
+ include/linux/byteorder/generic.h include/asm-generic/bitops/minix.h \
+ include/linux/log2.h include/linux/typecheck.h include/linux/printk.h \
+ include/linux/dynamic_debug.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/bug.h \
+ include/asm-generic/bug.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/div64.h \
+ include/asm-generic/percpu.h include/linux/threads.h \
+ include/linux/percpu-defs.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/system.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/cmpxchg.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/cmpxchg_32.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/nops.h \
+ include/linux/irqflags.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/irqflags.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/paravirt.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/pgtable_types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/pgtable_32_types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/pgtable-3level_types.h \
+ include/asm-generic/pgtable-nopud.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/paravirt_types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/desc_defs.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/kmap_types.h \
+ include/asm-generic/kmap_types.h include/linux/cpumask.h \
+ include/linux/bitmap.h include/linux/string.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/string.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/string_32.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/page.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/page_32.h \
+ include/asm-generic/memory_model.h include/asm-generic/getorder.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/msr.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/msr-index.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/errno.h \
+ include/asm-generic/errno.h include/asm-generic/errno-base.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/cpumask.h \
+ include/linux/personality.h include/linux/cache.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/cache.h \
+ include/linux/math64.h include/linux/err.h include/linux/spinlock.h \
+ include/linux/preempt.h include/linux/thread_info.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/thread_info.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/ftrace.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/atomic.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/atomic64_32.h \
+ include/asm-generic/atomic-long.h include/linux/bottom_half.h \
+ include/linux/spinlock_types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/spinlock_types.h \
+ include/linux/lockdep.h include/linux/rwlock_types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/spinlock.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/rwlock.h \
+ include/linux/rwlock.h include/linux/spinlock_api_smp.h \
+ include/linux/rwlock_api_smp.h include/linux/kdev_t.h \
+ include/linux/dcache.h include/linux/rculist.h include/linux/rcupdate.h \
+ include/linux/seqlock.h include/linux/completion.h \
+ include/linux/debugobjects.h include/linux/rcutree.h \
+ include/linux/rculist_bl.h include/linux/list_bl.h include/linux/path.h \
+ include/linux/stat.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/stat.h \
+ include/linux/time.h include/linux/radix-tree.h \
+ include/linux/prio_tree.h include/linux/pid.h include/linux/mutex.h \
+ include/linux/capability.h include/linux/semaphore.h \
+ include/linux/fiemap.h include/linux/quota.h include/linux/errno.h \
+ include/linux/rwsem.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/rwsem.h \
+ include/linux/percpu_counter.h include/linux/smp.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/smp.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/mpspec.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/mpspec_def.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/x86_init.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/bootparam.h \
+ include/linux/screen_info.h include/linux/apm_bios.h include/linux/edd.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/e820.h \
+ include/linux/numa.h include/linux/ioport.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/ist.h \
+ include/video/edid.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/apicdef.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/apic.h \
+ include/linux/delay.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/delay.h \
+ include/linux/pm.h include/linux/workqueue.h include/linux/timer.h \
+ include/linux/ktime.h include/linux/jiffies.h include/linux/timex.h \
+ include/linux/param.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/param.h \
+ include/asm-generic/param.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/timex.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/tsc.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/fixmap.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/acpi.h \
+ include/acpi/pdc_intel.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/numa.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/numa_32.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/mmu.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/io_apic.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/irq_vectors.h \
+ include/linux/percpu.h include/linux/pfn.h include/linux/dqblk_xfs.h \
+ include/linux/dqblk_v1.h include/linux/dqblk_v2.h \
+ include/linux/dqblk_qtree.h include/linux/nfs_fs_i.h include/linux/nfs.h \
+ include/linux/sunrpc/msg_prot.h include/linux/inet.h \
+ include/linux/fcntl.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/fcntl.h \
+ include/asm-generic/fcntl.h include/linux/namei.h \
+ include/linux/security.h include/linux/fsnotify.h \
+ include/linux/fsnotify_backend.h include/linux/idr.h \
+ include/linux/audit.h include/linux/elf-em.h include/linux/sched.h \
+ include/linux/rbtree.h include/linux/nodemask.h include/linux/mm_types.h \
+ include/linux/auxvec.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/auxvec.h \
+ include/linux/page-debug-flags.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/cputime.h \
+ include/asm-generic/cputime.h include/linux/sem.h include/linux/ipc.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/ipcbuf.h \
+ include/asm-generic/ipcbuf.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/sembuf.h \
+ include/linux/signal.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/signal.h \
+ include/asm-generic/signal-defs.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/siginfo.h \
+ include/asm-generic/siginfo.h include/linux/topology.h \
+ include/linux/mmzone.h include/linux/pageblock-flags.h \
+ include/generated/bounds.h include/linux/memory_hotplug.h \
+ include/linux/notifier.h include/linux/srcu.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/mmzone.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/mmzone_32.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/numaq.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/srat.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/topology.h \
+ include/asm-generic/topology.h include/linux/proportions.h \
+ include/linux/seccomp.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/seccomp.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/seccomp_32.h \
+ include/linux/unistd.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/unistd.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/unistd_32.h \
+ include/linux/rtmutex.h include/linux/plist.h include/linux/resource.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/resource.h \
+ include/asm-generic/resource.h include/linux/hrtimer.h \
+ include/linux/timerqueue.h include/linux/task_io_accounting.h \
+ include/linux/latencytop.h include/linux/cred.h include/linux/key.h \
+ include/linux/sysctl.h include/linux/selinux.h include/linux/aio.h \
+ include/linux/aio_abi.h include/linux/uio.h include/linux/slab.h \
+ include/linux/gfp.h include/linux/mmdebug.h include/linux/slub_def.h \
+ include/linux/kobject.h include/linux/sysfs.h include/linux/kobject_ns.h \
+ include/linux/kref.h include/linux/kmemleak.h include/linux/binfmts.h \
+ include/linux/shm.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/shmparam.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/shmbuf.h \
+ include/asm-generic/shmbuf.h include/linux/mm.h \
+ include/linux/debug_locks.h include/linux/range.h \
+ include/linux/bit_spinlock.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/pgtable.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/pgtable_32.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/pgtable_32_types.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/pgtable-3level.h \
+ include/asm-generic/pgtable.h include/linux/page-flags.h \
+ include/linux/huge_mm.h include/linux/vmstat.h include/linux/msg.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/msgbuf.h \
+ include/asm-generic/msgbuf.h include/linux/xfrm.h include/net/flow.h \
+ include/linux/in6.h include/linux/splice.h include/linux/pipe_fs_i.h \
+ include/linux/uaccess.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/uaccess.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/uaccess_32.h \
+ /root/aufs2-standalone.git/fs/aufs/aufs.h \
+ /root/aufs2-standalone.git/fs/aufs/debug.h include/linux/bug.h \
+ include/linux/module.h include/linux/kmod.h include/linux/elf.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/elf.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/user.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/user_32.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/vdso.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/desc.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/ldt.h \
+ include/linux/moduleparam.h include/linux/tracepoint.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/module.h \
+ include/asm-generic/module.h include/trace/events/module.h \
+ include/trace/define_trace.h include/linux/kallsyms.h \
+ include/linux/sysrq.h \
+ /root/aufs2-standalone.git/include/linux/aufs_type.h \
+ /root/aufs2-standalone.git/fs/aufs/branch.h include/linux/mount.h \
+ /root/aufs2-standalone.git/fs/aufs/dynop.h \
+ /root/aufs2-standalone.git/fs/aufs/inode.h \
+ /root/aufs2-standalone.git/fs/aufs/rwsem.h \
+ /root/aufs2-standalone.git/fs/aufs/super.h \
+ /root/aufs2-standalone.git/fs/aufs/spl.h \
+ /root/aufs2-standalone.git/fs/aufs/wkq.h \
+ /root/aufs2-standalone.git/fs/aufs/cpup.h \
+ /root/aufs2-standalone.git/fs/aufs/dcsub.h \
+ /root/aufs2-standalone.git/fs/aufs/dbgaufs.h \
+ /root/aufs2-standalone.git/fs/aufs/dentry.h \
+ /root/aufs2-standalone.git/fs/aufs/dir.h \
+ /root/aufs2-standalone.git/fs/aufs/file.h include/linux/poll.h \
+ /usr/src/linux-kernel-2.6.38.2/arch/x86/include/asm/poll.h \
+ include/asm-generic/poll.h /root/aufs2-standalone.git/fs/aufs/fstype.h \
+ include/linux/magic.h include/linux/romfs_fs.h \
+ /root/aufs2-standalone.git/fs/aufs/loop.h \
+ /root/aufs2-standalone.git/fs/aufs/module.h \
+ /root/aufs2-standalone.git/fs/aufs/opts.h \
+ /root/aufs2-standalone.git/fs/aufs/sysaufs.h \
+ /root/aufs2-standalone.git/fs/aufs/vfsub.h include/linux/lglock.h \
+ /root/aufs2-standalone.git/fs/aufs/whout.h
