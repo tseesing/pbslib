d8f3d40bfa3a3b25a6d82184cd3a338d0dfc0ca3
hzx <huangzhongxin@ivali.com>
2012-07-19 17:15:42 +0000

    add X11R7.7, xorg-server 1.12.2

--- office/thunderbird/files/vendor.js
+++ office/thunderbird/files/vendor.js
@@ -7,3 +7,6 @@ pref("mail.shell.checkDefaultMail", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
+
+// identify default locale to use for searchplugins
+pref("distribution.searchplugins.defaultLocale", "zh-CN");
